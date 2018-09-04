5fb0870fdd8bcb0870695e11bf79e918d80918a5
JeffAbney <jeffmabney@gmail.com>
2018-09-04 05:53:48 -0500

    add main input and output component, add style, connect input to output.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'; // <img src={logo} className="App-logo" alt="logo" />
 import './App.css';
 
 class App extends Component {
+  constructor (props){
+    super(props);
+
+    this.state = {
+      input: "Filler Text"
+    }
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+    handleChange(event){
+      this.setState({
+        input: event.target.value
+      });
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form className="Text-box Markdown-editor" id="editor">
+          <h1 className="Box-title">Editor</h1>
+          <textarea value={this.state.input} onChange={this.handleChange} className="User-input-text"/>
+        </form>
+        <div className="Text-box Markdown-preview" id="preview">
+          <h1 className="Box-title">Preview</h1>
+          <div className="Text-area">
+            <p>{this.state.input}</p>
+          </div> 
+        </div>
       </div>
     );
   }
